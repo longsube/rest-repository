1354965fc26d07632286e27c823e9175b3beb559
longlq <longsube@gmail.com>
2017-12-14 17:22:07 +0700

    create a simple Flask app

---
+++ item.py
@@ -0,0 +1,17 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+
+
+class ItemList(Resource):
+    def get(self):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+        items = []
+        for row in result:
+            items.append({'name': row[0], 'price': row[1]})
+        connection.close()
+
+        return {'items': items}
